d02e23b541c278e19eaa4ef5f65617cef43f5b1a
zhuruiyu <harvey_zry@icloud.com>
2016-12-09 21:59:38 +0800

    2016/12/9

--- 2.php
+++ 2.php
@@ -1,3 +1,26 @@
 <?php
-echo "hello world!";
+// for($i=1;$i<=9;$i++){
+// for($j=1;$j<=$i;$j++){
+// echo $i." * ".$j." = ".($i*$j)."&nbsp;";
+// }
+// echo "<br />";
+// }
+$color="";
+echo "<table width=1000 align='center' border='1'>";
+for($i=1;$i<=9;$i++){
+    if($i % 2==0){
+        $color="red";
+    }else{
+        $color="pink";
+    }
+    echo "<tr bgColor=".$color.">";
+    for($j=1;$j<=$i;$j++){
+        echo "<td>";
+        echo $i." * ".$j." = ".($i*$j)."&nbsp;";
+        echo "</td>";
+    }
+    echo "</tr>";
+}
+echo "<table>";
+
 ?>
---
+++ string.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/12/8 0008
+ * Time: 下午 7:05
+ */
+//echo"<pre>";
+// var_dump($_SERVER);
+//     echo"</pre>";
+
+//$str=$_SERVER['REQUEST_URI'];
+//
+//echo $str;
+
+
+//$arr=array('z','h','u');
+//$str="";
+//for($i=0;$i=count($arr);$i++){
+//$str.=$arr[$i];
+//}
+//
+//   echo $str;
+
+
+//$arr=array('z','h','u');
+//$str="";
+//for($i=0;$i<count($arr);$i++){
+//    $str.=$arr[$i];
+//}
+//echo $str;
+
+// $arr=array('l','a','o');
+// $str="";
+// for($i=0;$i<count($arr);$i++){
+// $str.=$arr[$i];
+// }
+//
+// echo $str;
